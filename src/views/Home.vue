94dac71b99b5c6988105733fff440be2dd2924c5
Anna Godek <anna.m.godek@gmail.com>
2024-04-23 18:14:39 +0200

    Statystyki na glownej (statyczne na razie)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,37 @@
 <template>
-      <div class="percent-tax-rate-box">
-        <div class="percent-tax-rate-title">Pomóż naszym zwierzakom przekazując 1,5% swojego podatku</div>
-        <div class="percent-tax-rate-images">
-            <img src="/kot_syjamski.png" alt="obrazek">
-            <img src="/pies1.jpg" alt="obraz2">
-            <img src="/pies3.jpg" alt="obraz3">   
+  <div class="percent-tax-rate-box">
+    <div class="percent-tax-rate-title">Pomóż naszym zwierzakom przekazując 1,5% swojego podatku</div>
+    <div class="percent-tax-rate-images">
+      <img src="/kot_syjamski.png" alt="obrazek">
+      <img src="/pies1.jpg" alt="obraz2">
+      <img src="/pies3.jpg" alt="obraz3">
+    </div>
+    <div class="KRS-info">KRS: 1234567899</div>
+  </div>
+
+  <div class="stats-container">
+    <div class="stats-wrapper">
+      <div class="percent-tax-rate-title">AdoPsiak w liczbach</div>
+        <div class="stats-row">
+          <div class="stats-item">
+            <font-awesome-icon icon="heart" size="2xl" style="color: #ffffff;" />
+            <p class="stats-value">321</p>
+            <p class="stats-label">zwierząt do adopcji</p>
+          </div>
+
+          <div class="stats-item">
+            <font-awesome-icon icon="hand-holding-dollar" size="2xl" style="color: #ffffff;" />
+            <p class="stats-value">258 035,25 zł</p>
+            <p class="stats-label">zebranych środków w ramach wsparcia</p>
+          </div>
+
+          <div class="stats-item">
+            <font-awesome-icon icon="house" size="2xl" style="color: #ffffff;" />
+            <p class="stats-value">1231</p>
+            <p class="stats-label">zwierząt znalazło swój dom</p>
+          </div>
+
         </div>
-        <div class="KRS-info">KRS: 1234567899</div>
-      </div> 
+      </div>
+    </div>
 </template>
