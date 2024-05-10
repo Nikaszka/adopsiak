a8b35f02aa3701e0a55153995a390793cf1215ea
Nikaszka <weronika.wojcik50@gmail.com>
2024-05-10 17:07:00 +0200

    edycja zdjęcia

--- src/views/EditAnimal.vue
+++ src/views/EditAnimal.vue
@@ -2,7 +2,7 @@
   <div class="edit-animal-container">
     <h1>Edytuj dane zwierzaka</h1>
     <div class="animal-details">
-        <div class="form-group">
+      <div class="form-group">
         <!-- <label for="genus">Płeć:</label> -->
         <select id="genus" v-model="animal.genus" class="form-select">
           <option value=0>Kot</option>
@@ -32,49 +32,103 @@
         <label for="localization">Lokalizacja:</label>
         <input type="text" id="localization" v-model="animal.localization" class="form-input" />
       </div>
+
+      <div class="image-container">
+        <div>
+          <label>Obecne zdjęcie:</label>
+          <img v-if="animal.animalPhotoId" :src='getPhoto(animal.animalPhotoId)' :alt="animal.name"
+            class="animal-image" />
+          <img v-if="!animal.animalPhotoId" :alt="animal.name" class="animal-image" />
+        </div>
+        <!-- Input for choosing a new image -->
+        <div>
+          <label for="image">Zdjęcie:</label>
+          <input type="file" id="image" ref="fileInput" accept="image/*" @change="handleFileChange">
+        </div>
+      </div>
+
+
       <div class="form-group">
         <label for="description">Opis:</label>
-        <textarea type="text" id="description" v-model="animal.description" class="form-input" > </textarea>
+        <textarea type="text" id="description" v-model="animal.description" class="form-input"> </textarea>
+      </div>
+
+      <div class="other-benefits">
+        <div class="animal-benefits">
+          <font-awesome-icon icon="heart" class="heart-icon" />
+          <input type="text" id="myInput" value="Uwielbia pieszczoty"></input>
+        </div>
+        <div class="animal-benefits">
+          <font-awesome-icon icon="heart" class="heart-icon" />
+          <input type="text" id="myInput" value="Uwielbia zabawę" class="gender-age"></input>
+        </div>
+        <div class="animal-benefits">
+          <font-awesome-icon icon="heart" class="heart-icon" />
+          <input type="text" id="myInput" value="Akceptuje inne zwierzęta"></input>
+        </div>
       </div>
       <div class="form-group">
-          <label for="childFriendly">Przyjazny dzieciom:</label>
-          <input type="checkbox" id="childFriendly" v-model="animal.childFriendly">
-        </div>
-        <div class="form-group">
-          <label for="catFriendly">Przyjazny kotom:</label>
-          <input type="checkbox" id="catFriendly" v-model="animal.catFriendly">
-        </div>
-        <div class="form-group">
-          <label for="dogFriendly">Przyjazny psom:</label>
-          <input type="checkbox" id="dogFriendly" v-model="animal.dogFriendly">
-        </div>
-        <div class="form-group">
-          <label for="likesToPlay">Lubi bawić się:</label>
-          <input type="checkbox" id="likesToPlay" v-model="animal.likesToPlay">
-        </div>
-        <div class="form-group">
-          <label for="likesToCuddle">Lubi przytulać się:</label>
-          <input type="checkbox" id="likesToCuddle" v-model="animal.likesToCuddle">
-        </div>
-        <div class="form-group">
-          <label for="doesntLikeShelter">Nie lubi schroniska:</label>
-          <input type="checkbox" id="doesntLikeShelter" v-model="animal.doesntLikeShelter">
-        </div>
-        <div class="form-group">
-        <label for="status">Status:</label> 
+        <label for="status">Status:</label>
         <select id="status" v-model="animal.status" class="form-select">
           <option value=0>Do adopcji</option>
           <option value=1>Adoptowany</option>
         </select>
-      <button @click="saveChanges" class="submit-button">Zapisz zmiany</button>
+        <button @click="saveChanges" class="submit-button">Zapisz zmiany</button>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import { defineProps } from 'vue'
+const fileInput = ref(null);
+let selectedFile = null;
+
+function handleFileChange(event) {
+  if (event) {
+    selectedFile = event.target.files[0];
+  } else {
+    selectedFile = null;
+  }
+}
+
+const deleteAnimalPhoto = async (id) => {
+  try {
+    const response = await fetch(`https://localhost:7241/Animals/${id}/deletePhoto`, {
+      method: 'DELETE',
+    });
+  } catch (error) {
+    if (error.status != 404) {
+      console.error('Error deleting animal photo:', error);
+    }
+  }
+
+};
+
+
+
+
+
+async function uploadImage() {
+  const imageData = new FormData();
+  imageData.append('photo', selectedFile);
+
+  try {
+    const response = await fetch(`https://localhost:7241/Animals/${props.id}/addPhoto`, {
+      method: 'POST',
+      body: imageData,
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to upload image.');
+    }
+
+  } catch (error) {
+    console.error('Error uploading image:', error);
+    alert('Failed to upload image.');
+  }
+}
 
 const props = defineProps(['id'])
 const animal = ref({
@@ -125,15 +179,28 @@ const saveChanges = async () => {
       throw new Error('Network response was not ok')
     }
     const data = await response.json()
+
+    if (selectedFile) {
+      if (animal.animalPhotoId) {
+        deleteAnimalPhoto(props.id)
+      }
+      uploadImage()
+    }
+
     console.log('Changes saved successfully:', data)
   } catch (error) {
     console.error('Error saving changes:', error)
   }
 }
+
+
+const baseUrl = 'https://localhost:7241/AnimalPhoto/';
+const getPhoto = (animalPhotoId) => {
+  return `${baseUrl}${animalPhotoId}`;
+};
 </script>
 
 <style scoped>
-
 .edit-animal-container {
   text-align: center;
 }
@@ -158,7 +225,7 @@ const saveChanges = async () => {
 }
 
 .form-select {
-  width: 100%; 
+  width: 100%;
 }
 
 .submit-button {
@@ -171,5 +238,81 @@ const saveChanges = async () => {
   font: 400 18px Inter, sans-serif;
   cursor: pointer;
 }
-</style>
 
+.submit-button:hover {
+  background-color: #d9d9d9;
+}
+
+
+.animal-image {
+  /* max-width: 100%; */
+  width: 130px;
+  margin-bottom: 20px;
+  padding-top: 10px;
+}
+
+.image-label {
+  display: block;
+  margin-bottom: 10px;
+}
+
+.image-container {
+  display: flex;
+}
+
+.image-container div input {
+  padding-left: 20px;
+  padding-top: 15px;
+  border: none;
+}
+
+textarea {
+  min-height: 100px;
+}
+
+
+.info-icon,
+.heart-icon {
+  font-size: 35px;
+}
+
+.heart-icon {
+  color: green;
+}
+
+.animal-form-title {
+  margin-left: 5px;
+  margin-right: 5px;
+  margin-bottom: 10px;
+}
+
+@media (max-width: 991px) {
+
+  .info-icon,
+  .heart-icon {
+    font-size: 18px;
+  }
+
+  .other-benefits {
+    margin-top: 10px;
+  }
+}
+
+.animal-benefits {
+  display: flex;
+  white-space: nowrap;
+  align-items: center;
+  padding: 10px;
+  gap: 15px;
+}
+
+input[type="text"],
+input[type="file"],
+textarea {
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+</style>
